96ccd83815bcfb4f577f7635184f78e2b051ecc3
mohanad badawy <mohanad20129876@gmail.com>
2022-11-24 19:36:04 +0200

    adding img to db

--- backend/models/product.js
+++ backend/models/product.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 let productSchema = new Schema(
   {
-    name: { type: String, required: true, unique: true },
+    name: { type: String, required: true, uniqe: true },
     imgURL: { type: String, required: true },
     price: { type: Number, required: true },
     desc: { type: String },
@@ -11,7 +11,12 @@ let productSchema = new Schema(
     inStock: { type: Boolean, default: true },
     color: { type: Array },
     code: { type: String },
+    size: { type: Array },
     rating: { type: Number },
+    discount: { type: Boolean },
+    discountPercent: { type: Number },
+    brand: { type: String },
+    tag: { type: Array },
   },
   { timestamps: true }
 );
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,8 +5,49 @@ const {
 } = require("./verifyToken");
 var router = require("express").Router();
 const Product = require("../models/product");
+const multer = require("multer");
+const crypto = require("crypto");
+const path = require("path");
+const Grid = require("gridfs-stream");
+const methodOverride = require("method-override");
+var mongoose = require("mongoose");
+const GridFsStorage = require("multer-gridfs-storage").GridFsStorage;
 
-router.post("/", adminVerifyToken, async (req, res) => {
+const { ReadStream } = require("fs");
+let gfs, gridfsBucket;
+router.use(methodOverride("_method"));
+
+const conn = mongoose.createConnection(process.env.DATABASE);
+conn.once("open", () => {
+  // Init stream
+  gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+    bucketName: "uploads",
+  });
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("uploads");
+});
+
+const storage = new GridFsStorage({
+  url: process.env.DATABASE,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: "uploads",
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
+});
+const upload = multer({ storage });
+
+router.post("/", adminVerifyToken, upload.single("img"), async (req, res) => {
   const newProduct = new Product(req.body);
   try {
     const savedProduct = await newProduct.save();
@@ -14,6 +55,9 @@ router.post("/", adminVerifyToken, async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+});
+router.post("/upload", upload.single("file"), (req, res) => {
+  res.send({ file: req.file });
 });
 
 router.put("/:id", adminVerifyToken, async (req, res) => {
@@ -49,15 +93,31 @@ router.get("/find/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+router.get("/image/:filename", (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    if (!file || file.length === 0) {
+      return res.status(404).json({ err: "noo file exists!" });
+    }
+    if (file.contentType === "image/jpeg" || file.contentType === "image/png") {
+      // Read output to browser
+      const readstream = gridfsBucket.openDownloadStreamByName(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: "Not an image",
+      });
+    }
+  });
+});
 
 router.get("/", async (req, res) => {
-  const qlast4 = req.query.last4;
+  const qlast6 = req.query.last6;
   const latest = req.query.latest;
   const qCategory = req.query.category;
   try {
     let products;
-    if (qlast4) {
-      products = await Product.find().sort({ createdAt: -1 }).limit(4);
+    if (qlast6) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(6);
     } else if (qCategory) {
       products = await Product.find({
         categories: {
